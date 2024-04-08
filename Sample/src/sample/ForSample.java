2b403eacfd1b6ccad74b1e7afa27f8b9a76956cd
ryohei.tada.997@dirbato.co.jp <dir-FY24tadaryohei>
2024-04-08 14:17:29 +0900

    Sample基礎

---
+++ Sample/src/sample/ForSample.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class ForSample {
+
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i+=2) {
+            if (i > 20) {
+                break;
+            }
+            System.out.println(i);
+        }
+
+    }
+    }
